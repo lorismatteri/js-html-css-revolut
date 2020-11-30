40388b21ecc028832d54e838e5b2faed046750b1
Loris Matteri <loris.matteri93@gmail.com>
2020-11-30 17:10:50 +0100

    Menu a tendina
    Inseriti div invisibili, che diventano visibili passando sopra col mouse, solo che si vedono tutti insieme e non uno per volta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log('ciaone');
+$(function() {
+    var menu = $('.to_open');
+
+    menu.each(function () { 
+        $(this).mouseenter(function () { 
+            $('.nav_right a').css('color', 'black');
+            $('.dropdown').css('opacity', '1');
+        });
+        $(this).mouseleave(function () { 
+            $('.nav_right a').css('color', 'grey');
+            $('.dropdown').css('opacity', '0');
+        });
+    });
+});
+
