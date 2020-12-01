21211c3dc4db0ffc14ee83bde13a1aef1555002d
Loris Matteri <loris.matteri93@gmail.com>
2020-12-01 09:24:21 +0100

    Tolta una variabile inutile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
 $(function() {
     var menu = $('.to_open');
     var drop = $('.dropdown');
-    var title = $('.nav_right a.to_open');
     
     menu.each(function (i, element) {
         // element == this
         $(this).hover(function () { 
-            $(title[i]).css('color', 'black');
+            $(menu[i]).css('color', 'black');
             $(drop[i]).css('opacity', '1');
         }, function() {
-            $(title[i]).css('color', 'grey');
+            $(menu[i]).css('color', 'grey');
             $(drop[i]).css('opacity', '0');
         });
     });
